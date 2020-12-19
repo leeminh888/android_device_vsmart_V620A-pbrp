1023e40a7888f26a6db6f40010a8641870a40394
leeminh888 <68751029+leeminh888@users.noreply.github.com>
2020-12-19 10:44:14 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,9 +107,33 @@ TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_MAX_BRIGHTNESS := 255
 TW_SCREEN_BLANK_ON_BOOT := true
 
-# PitchBlack Flags
-PB_DISABLE_DEFAULT_TREBLE_COMP := true
+# Path of your SHRP Tree
+SHRP_PATH := device/vsmart/V620A
 
-# Security Patch Hack to prevent Anti Rollback
-PLATFORM_SECURITY_PATCH := 2025-12-31
+# Maintainer name *
+SHRP_MAINTAINER := Leeminh
+
+# Device codename *
+SHRP_DEVICE_CODE := V620A
+
+# Recovery Type (It can be treble,normal,SAR) [Only for About Section] *
+SHRP_REC_TYPE := Normal
+
+# Recovery Type (It can be A/B or A_only) [Only for About Section] *
+SHRP_DEVICE_TYPE := A/B
+
+SHRP_OFFICIAL := true
+SHRP_EXPRESS := true
+SHRP_DARK := true
+SHRP_EDL_MODE := 1
+SHRP_EXTERNAL := /external_sd
+SHRP_INTERNAL := /sdcard
+SHRP_OTG := /usb_otg
+SHRP_FLASH := 1
+SHRP_STATUSBAR_RIGHT_PADDING := 40
+SHRP_STATUSBAR_LEFT_PADDING := 40
+SHRP_REC := /dev/block/bootdevice/by-name/recovery
+
+# Hack: prevent anti rollback
+PLATFORM_SECURITY_PATCH := 2099-12-31
 PLATFORM_VERSION := 16.1.0
